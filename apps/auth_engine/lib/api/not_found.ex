625768691495a47cbf5896ee0f7fa5022841d8e8
lite20 <lite260@gmail.com>
2018-03-11 16:36:14 -0600

    new 404

--- apps/auth_engine/lib/api/not_found.ex
+++ apps/auth_engine/lib/api/not_found.ex
@@ -5,6 +5,13 @@ defmodule Api.NotFound do
   def handle_request(_, _state) do
     response(:ok)
     |> set_header("content-type", "text/plain")
-    |> set_body("404 - No such endpoint")
+    |> set_body("""
+     _____     ______     ______   ______   ______
+    /\\  __-.  /\\  __ \\   /\\__  _\\ /\\__  _\\ /\\  __ \\
+    \\ \\ \\/\\ \\ \\ \\ \\/\\ \\  \\/_/\\ \\/ \\/_/\\ \\/ \\ \\ \\/\\ \\
+     \\ \\____-  \\ \\_____\\    \\ \\_\\    \\ \\_\\  \\ \\_____\\
+      \\/____/   \\/_____/     \\/_/     \\/_/   \\/_____/
+       404 - Not found. You sure that link is right?
+    """)
   end
 end
